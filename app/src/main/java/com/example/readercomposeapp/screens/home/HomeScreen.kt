d7ac0a0d6a0115dc29cad6a6bccd604b13a02651
polinas888 <p.shcherbinina@itransition.com>
2023-05-10 12:22:25 +0300

    profile view on home screen

--- app/src/main/java/com/example/readercomposeapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/readercomposeapp/screens/home/HomeScreen.kt
@@ -1,19 +1,28 @@
 package com.example.readercomposeapp.screens.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.readercomposeapp.R
 import com.example.readercomposeapp.components.BookSearcherTopAppBar
 import com.example.readercomposeapp.navigation.BookSearcherScreens
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -25,12 +34,68 @@ fun HomeScreen(navController: NavController) {
                 elevation = 0.dp,
                 iconColor = colorResource(R.color.light_blue),
                 title = "Book Searcher",
-                signout = {navController.navigate(BookSearcherScreens.LoginScreen.name)}
+                signout = { navController.navigate(BookSearcherScreens.LoginScreen.name) }
             )
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("Hello Scaffold", fontSize = 28.sp)
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(20.dp, 0.dp)
+                    .fillMaxWidth(),
+                Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Your reading\nactivity right now...", fontSize = 20.sp)
+                UserImageAndName(navController)
+            }
         }
+    }
+}
+
+@Composable
+private fun UserImageAndName(navController: NavController) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        UserImage(navController)
+        Text(
+            text = Firebase.auth.currentUser?.email?.split("@")?.get(0) ?: "No name",
+            modifier = Modifier.padding(4.dp),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.blue)
+        )
+    }
+}
+
+@Composable
+private fun UserImage(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .background(
+                colorResource(R.color.light_blue),
+                shape = CircleShape
+            )
+            .size(35.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.avatar_icon),
+            contentDescription = "avatar picture",
+            modifier = Modifier
+                .size(25.dp)
+                .align(Alignment.Center)
+                .clickable {
+                    navController.navigate(BookSearcherScreens.StatsScreen.name)
+                },
+            colorFilter = ColorFilter.tint(
+                color = Color.White
+            )
+        )
     }
 }
--- app/src/main/java/com/example/readercomposeapp/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/example/readercomposeapp/screens/stats/StatsScreen.kt
@@ -1,9 +1,10 @@
 package com.example.readercomposeapp.screens.stats
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun StatsScreen(navController: NavController) {
-
+    Text(text = "Stats screen")
 }
